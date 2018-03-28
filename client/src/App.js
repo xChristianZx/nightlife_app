749684e6996af3ea30858e2b90809267cba22eba
Christian Zenaty <cazenaty@yahoo.com>
2018-03-27 21:11:22 -0700

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import styles from "./App.css";
 import Search from "./containers/Search/Search";
+import NavBar from "./components/NavBar/NavBar";
 
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
+        <NavBar />
         <Search />
       </div>
     );
