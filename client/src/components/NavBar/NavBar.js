1ddd241f79a982f1bfd4f802bde0f678693895ac
Christian Zenaty <cazenaty@yahoo.com>
2018-03-27 21:11:09 -0700

    adding navbar

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <div className={styles.logoContainer}>
+          <h2>uNight</h2>
+        </div>
+
+        <ul className={styles.routesContainer}>
+          <li>
+            <a>Login</a>
+          </li>
+          <li>
+            <a>Register</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
