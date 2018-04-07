188db77ac9fc796909e0b1803104be5c4bd8b182
Christian Zenaty <cazenaty@yahoo.com>
2018-04-07 13:08:13 -0700

    adding redux logger

--- client/src/index.js
+++ client/src/index.js
@@ -7,12 +7,15 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
 import reduxThunk from "redux-thunk";
+import { createLogger } from "redux-logger";
+
+const logger = createLogger();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducers,
   {},
-  composeEnhancers(applyMiddleware(reduxThunk))
+  composeEnhancers(applyMiddleware(reduxThunk, logger))
 );
 
 ReactDOM.render(
