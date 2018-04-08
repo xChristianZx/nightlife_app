b6b29613dd3830127e65cde677c2bbe6f328d4c9
Christian Zenaty <cazenaty@yahoo.com>
2018-04-08 12:35:24 -0700

    quick fix

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -1,28 +1,26 @@
+const keys = require("../config/keys");
+const request = require("request");
+
 module.exports = app => {
   app.post("/", (req, res) => {
     const location = req.body.location;
     console.log(req.body);
     // console.log("location: ", req.body.location);
-
     const listSize = 10;
-
-    const YELP_API_ENDPOINT = "https://api.yelp.com/v3/businesses/search?";
+    const YELP_URL = "https://api.yelp.com/v3/businesses/search?";
     const uriOptions = `term=bars&location=${location}&limit=${listSize}`;
+    const baseURL = YELP_URL + uriOptions;
     const reqOptions = {
       auth: {
         bearer: keys.yelpAPIKEY
       }
     };
 
-    request.get(
-      YELP_API_ENDPOINT + uriOptions,
-      reqOptions,
-      (err, resp, body) => {
-        // console.log(err);
-        // console.log(resp.statusCode);
-        // console.log(body);
-        res.send(body);
-      }
-    );
+    request.get(baseURL, reqOptions, (err, resp, body) => {
+      // console.log(err);
+      // console.log(resp.statusCode);
+      // console.log(body);
+      res.send(body);
+    });
   });
 };
