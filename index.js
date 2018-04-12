f6eb3d36ab11dd3e62c04186263726af242546b5
Christian Zenaty <cazenaty@yahoo.com>
2018-04-12 14:49:20 -0700

    cleanup

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Cookie Session Config
 app.use(
   cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000, // 30days
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     keys: [keys.cookieKey]
   })
 );
