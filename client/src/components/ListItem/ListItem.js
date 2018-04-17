eb68d606f221b633a74a2e5866fe241799bd8cc5
Christian Zenaty <cazenaty@yahoo.com>
2018-04-16 19:01:33 -0700

    updated attendance handling

--- client/src/components/ListItem/ListItem.js
+++ client/src/components/ListItem/ListItem.js
@@ -1,9 +1,48 @@
 import React from "react";
 import styles from "./ListItem.css";
 
-const ListItem = ({ data, isLoggedIn, addUserToAttending }) => {
+const ListItem = ({ data, auth, addUserToAttending }) => {
   // id is yelp_id
   const { name, image_url, url, id, usersAttending } = data;
+  const { isLoggedIn, user } = auth;
+  const isAttending = () => {
+    if (isLoggedIn && user._id !== null) {
+      return usersAttending.includes(user._id) ? true : false;
+    }
+    return false;
+  };
+
+  const attendanceBtn = () => {
+    if (isLoggedIn && !isAttending()) {
+      return (
+        <button
+          className="tiny ui button primary"
+          onClick={() => addUserToAttending(id)}
+        >
+          RSVP YES!
+        </button>
+      );
+    } else if (isLoggedIn && isAttending()) {
+      return (
+        <div id={`${styles.attendance_btn}`} className={`ui animated button`}>
+          <div
+            className="visible content"
+            // onClick={() => removeUserFromAttending(id)}
+          >
+            Going!{"  "} <i className="icon check" />
+          </div>
+          <div className="hidden content">
+            Not Going {"  "}
+            <i className="icon times" />
+          </div>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  };
+
+  console.log(isAttending());
   return (
     <li className={styles.container}>
       <img src={image_url} alt={name} />
@@ -21,14 +60,7 @@ const ListItem = ({ data, isLoggedIn, addUserToAttending }) => {
             <i className="user icon" />
             {usersAttending.length} Going
           </span>
-          {isLoggedIn ? (
-            <button
-              className="tiny ui button primary"
-              onClick={() => addUserToAttending(id)}
-            >
-              I'm Going!
-            </button>
-          ) : null}
+          {attendanceBtn()}
         </div>
       </div>
     </li>
